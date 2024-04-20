454f580e9c1f9fdaa69773c450814ecaec7920fd
dpohvar <dpohvar@gmail.com>
2024-04-20 06:48:09 +0300

    fix types 1.0.2

--- modules.d.ts
+++ modules.d.ts
@@ -1,5 +1,3 @@
-export * from "./src/index.js"
-
 declare module "varhub:room" {
 	type RoomEvents = {
 		join: [string];
---
+++ types.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="./modules.d.ts" />
+
+export * from "./src/index"
